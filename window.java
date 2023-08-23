ac69142cd75168d02ea3f721d462b198c17f9e4f
c3kimball <c3kimball@student.bridgew.edu>
2023-08-23 12:36:12 -0400

    Adding a conditions text file and moving forward with all text areas

--- window.java
+++ window.java
@@ -9,9 +9,24 @@ public class window {
         JFrame frame = new JFrame("GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JButton square = new JButton("Click here");
-        square.setBounds(120, 0, 120, 120);
-        frame.add(square);
+        JTextArea field = new JTextArea("A player killed 2 or more monsters  in the same turn");
+        field.setLineWrap(true);
+        frame.add(field);
+
+        field.setBounds(10,0, 100, 100);
+
+        JTextArea field1 = new JTextArea("The top of the treasure deck is placed on the bottom of the treasure deck");
+        field1.setLineWrap(true);
+        frame.add(field1);
+
+        field1.setBounds(120,0, 100, 100);
+
+
+//        String buttonText = new String("A player killed 2 monsters in 1 turn");
+//        JButton square = new JButton();
+//        square.setBounds(120, 0, 120, 120);
+//        frame.add(square);
+
 
         //To display the window
         frame.setSize(800,600);
